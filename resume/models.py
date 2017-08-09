5070a0ca9ca7b035d78ed60ff9921f06d816bdec
rajats-smpx <rajat.srivastava@smartprix.com>
2017-08-09 16:55:05 +0530

    Models created

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,61 @@
+import os
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
-# Create your models here.
+class Details(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    contact = models.CharField(max_length=10)
+    address = models.CharField(max_length=500)
+    facebook = models.CharField(max_length=100)
+    github = models.CharField(max_length=100)
+    linkedin = models.CharField(max_length=100)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'details'
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+	def save(self, *args, **kwargs):
+		if not self.createdAt:
+			self.createdAt = timezone.now()
+
+		self.updatedAt = timezone.now()
+		return super(Review, self).save(*args, **kwargs)
+
+class Experience(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.CharField(max_length=200)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    description = models.TextField(max_length=1000)
+
+class Education(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    institution_name = models.CharField(max_length=100)
+    start_year = models.CharField(max_length=4)
+    end_year = models.CharField(max_length=4)
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000, null=True, blank=True)
+    url = models.CharField(null=True, blank=True, max_length=500)
+    image_file = models.FileField(blank=True)
+
+    class Meta:
+        unique_together = ['user', 'name']

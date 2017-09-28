39c01a8527a74497b6cdb799b7cc56fd4c8832a6
rajats-smpx <rajat.srivastava@smartprix.com>
2017-09-28 13:14:28 +0530

    Added .pylintrc

--- biografya/urls.py
+++ biografya/urls.py
@@ -7,6 +7,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='website/index.html'), name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts/urls')),
     url(r'^resume/', include('resume.urls')),
     url(r'^website/', include('website.urls')),
 ]
